86295ef25d7544b150f5dbff2c7afa486da0f36b
Feng J <ifengji@outlook.com>
2020-08-04 09:20:46 -0700

    Add files via upload

---
+++ IVCV/R/pack.R
@@ -0,0 +1,26 @@
+# library(devtools)
+# library(roxygen2)
+# library(usethis)
+# library(tidyverse)
+# library(IVCV)
+# use_r("cond_dis")
+# use_r("cv_splitter")
+# use_r("sim_IV")
+# use_r("rep_row")
+# use_r("rep_col")
+# use_r("demo_RE")
+# use_r("threshold")
+# use_testthat()
+# load_all()
+# use_r("hello")
+#
+# # recompile after add comments (roxygen2)
+# document()
+# rm(list = c("hello"))
+#
+# # build the package
+# install()
+#
+#
+#
+# threshold(1, .5, 2, 2)
